f815f99ded6dde9dca1d1d39343cc663fff0906b
bpaksoy <paksoybanu83@gmail.com>
2018-04-29 13:08:39 -0400

    routes fixed

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 
+
 // const ejs = require('ejs');
 // app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 const bodyParser = require('body-parser');
-app.use(express.static( "public" ));
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -15,13 +22,13 @@ app.get("/", function(req, res, next){
   res.render("index");
 });
 app.get("/about_us", function(req, res, next){
-  res.render("about_us");
+  res.render("about_us.html");
 });
 app.get("/contact", function(req, res, next){
-  res.render("contact");
+  res.render("contact.html");
 });
 app.get("/projects", function(req, res, next){
-  res.render("projects");
+  res.render("projects.html");
 });
 
 app.listen(port, function(){
